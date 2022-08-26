60df44b8621f21dda8a9401aa61638bcc234b0dc
aniede <ndubest56@gmail.com>
2022-08-26 06:24:52 +0100

    Added more ordering functions

--- Ordering App/order_app.rb
+++ Ordering App/order_app.rb
@@ -6,9 +6,10 @@ food2 = Food.new(name: "Beans", price: 20, calorie: 600)
 food3 = Food.new(name: "Akpo", price: 10, calorie: 1000)
 drink1 = Drink.new(name: "Beer", price: 30, volume: 300)
 drink2 = Drink.new(name: "Soft D", price: 45, volume: 400)
+dring2 = Drink.new(name: "Malt", price: 50, volume: 450)
 
 menus = [food1, food2, food3, drink1, drink2,]
-index = 1
+index = 0
 
 menus.each do |menu|
 	puts "No. #{index}. #{menu.info} each"
@@ -20,7 +21,9 @@ name = gets.chomp
 puts "Hello #{name}, Welcome to Younglife Store"
 
 puts "select purchase item by number"
+
 order = gets.chomp.to_i
+
 if order > menus.length
 	puts "please select No from the list"
 else
@@ -29,7 +32,7 @@ order_selected = menus[order]
 
 puts "you have selected: #{order_selected.name}"
 
-puts "How many do you want to purchase?"
+puts "How many do you want to purchase? (Buy from 3 and above and get 1$ discount)"
 
 times = gets.chomp.to_i
 
